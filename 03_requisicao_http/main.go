578ea708f2ea36fa30a93b48cb0496b7e15973f7
Claudio Santos <cljos@hotmail.com>
2023-04-24 10:21:44 -0300

    03 - http requisitions

---
+++ 03_requisicao_http/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	req, err := http.Get("https://www.uol.com.br/")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer req.Body.Close()
+
+	res, err := io.ReadAll(req.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	println(string(res))
+
+}
